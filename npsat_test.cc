70c13cecf22c57ef4e6aa90836a5f70b25c14071
George Kourakos <giorgk@gmail.com>
2018-01-17 09:49:11 -0800

    Test Commit first file

---
+++ npsat_test.cc
@@ -0,0 +1,32 @@
+#include <deal.II/base/logstream.h>
+#include <deal.II/base/mpi.h>
+#include <deal.II/base/conditional_ostream.h>
+
+#include <iostream>
+#include <list>
+
+#include "myheaders/user_input.h"
+#include "myheaders/dsimstructs.h"
+#include "myheaders/cgal_functions.h"
+#include "myheaders/interpinterface.h"
+#include "myheaders/npsat.h"
+
+using namespace dealii;
+
+int main (int argc, char **argv){
+    deallog.depth_console (1);
+    Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+
+    CL_arguments<2> CLI;
+    if (CLI.parse_command_line(argc,argv)){
+        CLI.read_param_file();
+        CLI.Debug_Prop();
+        NPSAT<2> npsat(CLI.AQprop);
+    }
+
+    InterpInterface<2> II;
+    II.get_data("10");
+    std::cout << II.interpolate(Point<2>(0,0)) << std::endl;
+
+	return 0;
+}
