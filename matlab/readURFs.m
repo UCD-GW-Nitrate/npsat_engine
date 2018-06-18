21b05d978dd0dd2c538d59d78e8a0a8a5c32ca5a
George Kourakos <giorgk@gmail.com>
2018-06-18 14:57:18 -0700

    update the ComputeURF matlab function

--- matlab/readURFs.m
+++ matlab/readURFs.m
@@ -6,29 +6,34 @@ topt.Ttime = 200; % [years]
 
 fid = fopen(filename,'r');
 cnter = 1;
-prev_Eid = -9;
-while ~feof(fid)
-    C = fscanf(fid,'%d',3);
-    E_id = C(1);
-    Np = C(3);
+
+%clf
+%hold on
+
+while 1
+    temp = fgetl(fid);
+    if temp == -1
+        break;
+    end
+    C = textscan(temp,'%d',3);
+    E_id = C{1}(1);
+    S_id = C{1}(2);
+    fprintf('%d %d\n',[E_id S_id])
+    Np = C{1}(3);
     C = fscanf(fid,'%f',Np*4);
+    temp = fgetl(fid);
     C = reshape(C,4,Np)';
+    %plot3(C(:,1),C(:,2),C(:,3),'.')
     urf = ComputeURF(C(:,1:3), C(:,4), topt);
     
-
-    if prev_Eid ~= E_id
-        if prev_Eid ~= -9
-            display(prev_Eid);
-            WellURF(cnter,1).id = prev_Eid;
-            WellURF(cnter,1).URF = URF;
-            cnter = cnter + 1;
-        end
-        prev_Eid = E_id;
-        URF = urf;
-    else
-        URF = [URF; urf];
-    end
-        
-
+    WellURF(cnter,1).Eid = E_id;
+    WellURF(cnter,1).Sid = S_id;
+    WellURF(cnter,1).p_cds = C(1,1:3);
+    WellURF(cnter,1).v_cds = C(1,4);
+    WellURF(cnter,1).p_lnd = C(end,1:3);
+    WellURF(cnter,1).v_lnd = C(end,4);
+    WellURF(cnter,1).URF = urf;
+    
+    cnter = cnter + 1;
 end
 fclose(fid);
