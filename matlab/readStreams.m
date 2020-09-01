6e42804f3cfd3e3efabc1c236cfb54dc3f60fdea
George Kourakos <giorgk@gmail.com>
2020-09-01 05:30:40 +0300

    Update matlab scripts

--- matlab/readStreams.m
+++ matlab/readStreams.m
@@ -1,4 +1,4 @@
-function STRM = readStreams(filename)
+function STRM = readStreams(filename, varargin)
 % STRM = readStreams(filename) reads the streams from the NPSAT input file
 %
 % filename is the name of the input stream file
@@ -8,15 +8,24 @@ function STRM = readStreams(filename)
 %      Q:       the rate, which should be in [L/T]
 %      area:    the area of the stream segment. This is not read but
 %               calculated by the coordinates in poly
+%      info:    Any other property that may be associated with the river
+%               polygons
+% Format of the information
+if nargin == 1
+    frmt = '%d %f';
+else
+    frmt = ['%d %f ' varargin{1}];
+end
 
-
+disp(nargin)
 fid = fopen(filename,'r');
 temp = fscanf(fid, '%d',1);
 Nstrm = temp(1);
 for ii = 1:Nstrm
-    temp = textscan(fid, '%d %f',1);
+    temp = textscan(fid, frmt,1);
     Np = temp{1,1}(1);
     q = temp{1,2}(1);
+    info =cell2mat(temp(3:end));
     poly = nan(Np,2);
     temp = textscan(fid, '%f %f',Np);
     poly(:,1) = temp{1,1};
@@ -29,6 +38,7 @@ for ii = 1:Nstrm
     STRM(ii,1).poly = poly;
     STRM(ii,1).Q = q;
     STRM(ii,1).area = polyarea(poly(:,1), poly(:,2));
+    STRM(ii,1).info = info;
 end
 
 fclose(fid);
--- matlab/readURFs_ichnos.m
+++ matlab/readURFs_ichnos.m
@@ -34,7 +34,7 @@ function WellURF = readURFs_ichnos(filename, opt)
         end
     end
 
-
+    cnt_urfs = 0;
     fid = fopen(filename,'r');
     if fid < 0
         WellURF = [];
@@ -59,7 +59,7 @@ function WellURF = readURFs_ichnos(filename, opt)
         end
         end_of_streamline = false;
         pp = nan(5000,3);
-        vv = nan(5000,3);
+        vv = nan(5000,1);
         cnt = 1;
         while end_of_streamline == false
             C = textscan(temp,'%f',9);
@@ -70,19 +70,26 @@ function WellURF = readURFs_ichnos(filename, opt)
                 ex_r = parseExitReason(C{4});
                 break;
             end
-            pp(cnt,:) = C{1}(4:6);
-            vv(cnt,:) = C{1}(7:9);
+            pp(cnt,:) = C{1}(3:5);
+            vv(cnt,:) = C{1}(6);
             cnt = cnt + 1;
             temp = fgetl(fid);
         end
         pp(cnt:end,:) = [];
         vv(cnt:end,:) = [];
-        fprintf('%d %d\n',[E_id S_id])
+        cnt_urfs = cnt_urfs + 1;
+        fprintf('%d %d %d\n',[E_id S_id cnt_urfs])
         vv = vv./topt.mult;
-        
+        %if cnt_urfs > 200
+        %    break;
+        %end
         %plot3(C(:,1),C(:,2),C(:,3),'.')
         if true_mode
             id_rmv = sqrt(sum(pp(:,1:2).^2,2)) < 10;
+            pp(id_rmv,:) = [];
+            vv(id_rmv,:) = [];
+            %Find those that are identical
+            id_rmv = find(sqrt(sum(diff(pp).^2,2)) <0.001);
             pp(id_rmv,:) = [];
             vv(id_rmv,:) = [];
             %if sqrt(sum(C(:,1:3).^2)) < 0.1
@@ -90,20 +97,36 @@ function WellURF = readURFs_ichnos(filename, opt)
             %    warning('The first point of streamline is (0 0 0) and will be removed')
             %end
             vv  = sqrt(sum(vv.^2,2));
-            [urf, ~] = ComputeURF(pp, vv, topt);
+            
             WellURF(cnter,1).Eid = E_id;
             WellURF(cnter,1).Sid = S_id;
             WellURF(cnter,1).Exit = ex_r;
-            WellURF(cnter,1).p_cds = pp(1,:);
-            WellURF(cnter,1).v_cds = vv(1);
-            WellURF(cnter,1).p_lnd = pp(end,:);
-            WellURF(cnter,1).v_lnd = vv(end);
-            L = cumsum(sqrt(sum((pp(2:end,:) - pp(1:end-1,:)).^2,2)));
-            WellURF(cnter,1).L = L(end);
-            WellURF(cnter,1).Age = sum(diff([0;L])./vv(1:end-1))/365;
-            % WellURF(cnter,1).v_eff = v_eff(1);
-            % WellURF(cnter,1).v_m = v_eff(2);
-            WellURF(cnter,1).URF = urf;
+            if ex_r == 1 || ex_r == 2  || ex_r == 3
+                WellURF(cnter,1).p_cds = pp(1,:);
+                WellURF(cnter,1).v_cds = vv(1);
+                WellURF(cnter,1).p_lnd = pp(end,:);
+                WellURF(cnter,1).v_lnd = vv(end);
+                L = cumsum(sqrt(sum((pp(2:end,:) - pp(1:end-1,:)).^2,2)));
+                WellURF(cnter,1).L = L(end);
+                WellURF(cnter,1).Age = sum(diff([0;L])./vv(1:end-1))/365;
+                % WellURF(cnter,1).v_eff = v_eff(1);
+                % WellURF(cnter,1).v_m = v_eff(2);
+                topt.Ttime = min(1000,round(4*sum(diff([0;L])./vv(1:end-1))/365));
+                topt.Ttime = ceil(topt.Ttime/100)*100;
+                %display(topt.Ttime)
+                urf = ComputeURF(pp, vv, topt);
+                %plot(urf)
+                %drawnow
+                WellURF(cnter,1).URF = urf;
+            else
+                WellURF(cnter,1).p_cds = pp(1,:);
+                WellURF(cnter,1).v_cds = vv(1);
+                WellURF(cnter,1).p_lnd = nan;
+                WellURF(cnter,1).v_lnd = nan;
+                WellURF(cnter,1).L = nan;
+                WellURF(cnter,1).Age = nan;
+                WellURF(cnter,1).URF = [];%%%%%%urf;
+            end
             cnter = cnter + 1;
             if cnter > size(WellURF,1)
                 WellURF = allocate_space(WellURF);
@@ -155,10 +178,18 @@ end
 function id = parseExitReason(er)
     if strcmp(er,'EXIT_TOP')
         id = 1;
+    elseif strcmp(er, 'EXIT_SIDE')
+        id = 2;
     elseif strcmp(er,'EXIT_BOTTOM')
         id = 3;
+    elseif strcmp(er,'CHANGE_PROCESSOR')
+        id = 0;
+    elseif strcmp(er,'INIT_OUT')
+        id = 4;
+    elseif strcmp(er,'MAX_INNER_ITER')
+        id = 7;
     elseif strcmp(er,'STUCK')
-        id = 9;
+        id = 5;
     else
         id = 0;
        warning(['Uknown Exit Reason ', er]);
